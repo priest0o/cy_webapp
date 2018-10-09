c93d5364d0632223eb0cee10d39b5442bc2d45b2
cy <ym123126@163.com>
2018-10-09 10:13:30 +0800

    修改web to app

--- www/orm.py
+++ www/orm.py
@@ -10,10 +10,11 @@ import aiomysql
 def log(sql, args=()):
     logging.info('SQL: %s' % sql)
 
-async def create_pool(loop, **kw):
+@asyncio.coroutine
+def create_pool(loop, **kw):
     logging.info('create database connection pool...')
     global __pool
-    __pool = await aiomysql.create_pool(
+    __pool = yield from aiomysql.create_pool(
         host=kw.get('host', 'localhost'),
         port=kw.get('port', 3306),
         user=kw['user'],
