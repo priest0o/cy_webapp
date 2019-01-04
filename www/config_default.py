488f3034352eb6187cd5fa4da186d30d3c58f2ee
cy <chenyang_rd@netposa.com>
2019-01-04 17:30:29 +0800

    all

---
+++ www/config_default.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+__author__ = 'cy'
+
+configs = {
+	'db':{
+		'host': 'localhost',
+		'port': 3306,
+		'user': 'root',
+		'password': '123456',
+		'db': 'awesome'
+	},
+	'session':{
+		'secret': 'AwEsOmE'
+	}
+}
--- www/do_it.py
+++ www/do_it.py
@@ -4,11 +4,11 @@ __author__ = 'cy'
 
 
 import orm,asyncio
-from models import User,Blog,Comment
+from models import User
 
 async def test(loop):
     await orm.create_pool(loop,user='www-data',password='www-data',db='awesome')
-    u = User(name='Test2',email='test2@example.com',passwd='1234567890',image='about:blank')
+    u = User(name='Test6', email='test6@example.com',passwd='1234567890',image='about:blank')
     await u.save()
 
 async def find(loop):
@@ -18,7 +18,7 @@ async def find(loop):
 
 loop = asyncio.get_event_loop()
 # loop.run_until_complete(asyncio.wait([test(loop),find(loop)]))
-loop.run_until_complete(find(loop))
+loop.run_until_complete(test(loop))
 loop.run_forever()
 
 
@@ -37,4 +37,23 @@ loop.run_forever()
 # loop.run_forever()
 # loop.close()
 # print ('test end')
+
+# from coroweb import get
+# from models import User
+# import asyncio
+#
+# @get('/')
+# async def index(request):
+# 	users = await User.findAll()
+# 	# 视图函数返回的值是dict
+# 	return {
+# 		# 在response_middleware中会搜索模板
+# 		'__template__': 'test.html',
+# 		# 传入模板的数据
+# 		'users': users
+# 	}
+#
+# @get('/hello')
+# async def hello(request):
+# 	return '<h1>hello!</h1>'
 
